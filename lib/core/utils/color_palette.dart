8900716df26f917f5e753b81400f2b17135270fd
jujuupanda <jujuupanda@gmail.com>
2024-11-10 19:03:39 +0700

    design dashboard and profile

---
+++ lib/core/utils/color_palette.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  Color white = const Color(0xFFf5f5f5);
+  Color black = const Color(0xFF050505);
+  Color grayToWhite = const Color(0xFFeaeaea);
+  Color softBlue1 = const Color(0xFF92b6f0);
+  Color softBlue2 = const Color(0xFFa0bff0);
+  Color softBlue3 = const Color(0xFFb2cbf2);
+  Color softBlue4 = const Color(0xFFc2d6f6);
+  Color softBlue5 = const Color(0xFFd1dff6);
+  Color greenPresence = const Color(0xFF3f9c11);
+  Color redPresence = const Color(0xFF9c2f11);
+  Color redWarning = const Color(0xFFf72e2e);
+  Color yellowPresence = const Color(0xFF9c8c11);
+
+
+}
