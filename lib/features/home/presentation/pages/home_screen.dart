03228d10401a1b36be1709340400bfe740b888a8
jujuupanda <jujuupanda@gmail.com>
2024-11-10 10:44:55 +0700

    a

--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -12,7 +12,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Home"),
+        child: Text("Homes"),
       ),
     );
   }
